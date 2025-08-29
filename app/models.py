7868af45c6c6307b9962e17e02a86ad662d4b90f
KirillGusev0 <43314984+KirillGusev0@users.noreply.github.com>
2025-08-29 14:01:22 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,21 +1,22 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# Схема для сущности (например, "Item")
+# Схема для сущности
 class Item(BaseModel):
     id: int
     name: str
     description: Optional[str] = None
     price: float
 
-# Схема для создания (id не передаётся — назначаем сами)
+# Схема для создания 
 class ItemCreate(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
 
-# Схема для обновления (все поля опциональны)
+# Схема для обновления 
 class ItemUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     price: Optional[float] = None
+
